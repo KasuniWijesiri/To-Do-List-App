74b4ff4fea51a3c61dcb0c43622d0bc0ab2cbe4d
KasuniWijesiri <152061729+KasuniWijesiri@users.noreply.github.com>
2024-05-10 12:00:36 +0530

    created AddTask,MainActivity,Task,TaskDatabaseHelper,activity_add_task.xml and activity_main.xml

--- app/src/main/java/com/example/to_do/AddTask.kt
+++ app/src/main/java/com/example/to_do/AddTask.kt
@@ -1,20 +1,38 @@
 package com.example.to_do
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.to_do.databinding.ActivityAddTaskBinding
 
 class AddTask : AppCompatActivity() {
+
+    private lateinit var binding:ActivityAddTaskBinding
+    private lateinit var db:TaskDatabaseHelper
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_add_task)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding=ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db= TaskDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener{
+            val title=binding.titleEditText.text.toString()
+            val content=binding.contentEditText.text.toString()
+            val task=Task(0,title,content)
+            db.insertTask(task)
+            finish()
+            Toast.makeText(this,"Task saved",Toast.LENGTH_SHORT).show()
+
+
+
+
+        }
+
         }
     }
-}
